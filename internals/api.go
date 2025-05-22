c28289b33fdcee4bd469c9c7834e844ca8cc9979
prathamVayana <pratham.mehta@vayana.com>
2025-05-22 17:38:37 +0530

    handled stack trace exceptions along with proper error response

--- internals/api.go
+++ internals/api.go
@@ -1,7 +1,9 @@
 package internals
 
 import (
+	"fmt"
 	"net/http"
+	"runtime/debug"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,32 +16,47 @@ type HandlerFunc func(AppContext, *gin.Context) (any, error)
 
 func HandlerWrapper(appCtx AppContext, handlerf HandlerFunc) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var res any
+		defer func() {
+			switch exception := recover(); exception {
+			case nil:
+				var statusCode int = http.StatusInternalServerError
+				switch c.Request.Method {
+				case http.MethodPut, http.MethodPatch:
+					statusCode = http.StatusNoContent
+				case http.MethodPost:
+					statusCode = http.StatusCreated
+				case http.MethodDelete:
+					statusCode = http.StatusNoContent
+				default:
+					statusCode = http.StatusOK
+				}
+
+				switch v := res.(type) {
+				case nil:
+					c.Status(http.StatusNoContent)
+				case gin.H:
+					c.JSON(statusCode, v)
+				case string:
+					c.String(statusCode, v)
+				default:
+					c.JSON(statusCode, v)
+				}
+			default:
+				stack := debug.Stack()
+				fmt.Printf("Panic recovered: %v\nStack trace:\n%s\n", exception, stack)
+				c.AbortWithStatusJSON(http.StatusInternalServerError, ErrorResponse{Error: "Internal Server Error"})
+			}
+		}()
+
 		res, err := handlerf(appCtx, c)
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		}
-
-		var statusCode int = http.StatusInternalServerError
-		switch c.Request.Method {
-		case http.MethodPut, http.MethodPatch:
-			statusCode = http.StatusNoContent
-		case http.MethodPost:
-			statusCode = http.StatusCreated
-		case http.MethodDelete:
-			statusCode = http.StatusNoContent
-		default:
-			statusCode = http.StatusOK
-		}
-
-		switch v := res.(type) {
-		case nil:
-			c.Status(http.StatusNoContent)
-		case gin.H:
-			c.JSON(statusCode, v)
-		case string:
-			c.String(statusCode, v)
-		default:
-			c.JSON(statusCode, v)
+			c.JSON(http.StatusInternalServerError, ErrorResponse{Error: err.Error()})
+			return
 		}
 	}
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
 }
